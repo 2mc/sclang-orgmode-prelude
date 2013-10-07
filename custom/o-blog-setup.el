6277d57bcf8d91fcf35f05617eee7c084229d254
Ioannis Zannos <zannos@gmail.com>
2013-10-07 14:21:50 +0300

    + o-journal submodule, o-blog-setup, htmlize

---
+++ custom/o-blog-setup.el
@@ -0,0 +1,12 @@
+;;; o-glob-setup.el --- Load packages for using o-blog
+
+;;; Commentary: 
+;;; o-blog is a package for publishing static web sites from orgmode.
+;;; By Sébastien Gross (renard)
+;;; See: https://github.com/renard/o-blog, http://renard.github.io/o-blog/
+
+;;; Code:
+
+(require 'htmlize)
+
+(require 'o-blog)
