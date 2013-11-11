05c528392e7c4c6d5508aa0859605446614f7ef0
mcGitGo <mc@aiguphonie.com>
2013-11-11 14:18:11 +0200

    tested against own prelude tag 'rebuilt1.0'

--- custom-elisp/macros.el
+++
@@ -1,9 +0,0 @@
-
-;;; Keyboard macros defined during work
-
-(fset 'org-demote-next-node
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([14 M-S-left] 0 "%d")) arg)))
-
-(fset 'increment-r-and-insert
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("r+RriR" 0 "%d")) arg)))
-
